a7d686bc2fdee66df07b413c81ef32b4fc8422bf
Saif Minhas <saif_minhas@hotmail.com>
2020-04-13 14:46:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |valid_calls|
+  planeteer_calls.find do |planteer_call|
   valid_calls.include?(planeteer_calls)
  
 end
